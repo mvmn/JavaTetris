68c0bad9cebeb2fbd12dae4c3a493a8bc5626e9f
Mykola Makhin <mvmn.inbox@gmail.com>
2022-09-18 13:19:35 +0300

    Version 1: fully procedural Tetris.

---
+++ src/main/java/x/mvmn/games/tetris/Фігури.java
@@ -0,0 +1,41 @@
+package x.mvmn.games.tetris;
+
+public class Фігури {
+
+  public static boolean[][][] ФІГУРИ = { //
+      { //
+          {true, true}, //
+          {true, true} //
+      }, //
+      { //
+          {true}, //
+          {true}, //
+          {true}, //
+          {true} //
+      }, //
+      { //
+          {true, true}, //
+          {true, false}, //
+          {true, false} //
+      }, //
+      { //
+          {true, true}, //
+          {false, true}, //
+          {false, true} //
+      }, //
+      { //
+          {false, true, false}, //
+          {true, true, true} //
+      }, //
+      { //
+          {true, false}, //
+          {true, true}, //
+          {false, true} //
+      }, //
+      { //
+          {false, true}, //
+          {true, true}, //
+          {true, false} //
+      } //
+  };
+}
