168e735e65b6c52d6792dd263b4036d9404dd8e4
Mykola Makhin <mvmn.inbox@gmail.com>
2022-10-29 10:55:43 +0300

    Pretty render in procedural tetris

--- src/main/java/x/mvmn/games/tetris/procedural/ProceduralTetris.java
+++ src/main/java/x/mvmn/games/tetris/procedural/ProceduralTetris.java
@@ -68,6 +68,11 @@ public class ProceduralTetris {
 
             @Override
             public void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
                 відмальовка(g);
                 Toolkit.getDefaultToolkit().sync();
             }
